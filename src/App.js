818220f020be3a8af7063504f0c0488dc76ad136
David Coronado <david.coronado@escuelaing.edu.co>
2021-10-27 23:53:17 -0500

    add submit task

--- src/App.js
+++ src/App.js
@@ -18,18 +18,40 @@ function App() {
       isCompleted: false,
       name: "Keep on Keeping on"
     },
-  ]);
+  ]);  
+
+  const [textValue, setTextValue] = useState("");
 
   const handleTaskChange = (index) => () => {
     const arr = [...tasks];
     arr[index].isCompleted = !arr[index].isCompleted;
     setTasks(arr);
+  }; 
+
+  const newTask = (name) => {
+    const newTask = {
+      isCompleted: false,
+      name:name,
+    }; 
+    setTasks([...tasks,newTask]);
+
   };
+
+
+  const handleSubmit = (event) =>{
+    event.preventDefault(); 
+    newTask(textValue);
+  } 
+
+  const handleTextChange = (event)=>{
+    const value = event.target.value; 
+    setTextValue(value);
+  } 
 
   return (
     <main> 
-        <form>
-          <input type="text" placeholder="Task name"/>
+        <form onSubmit={handleSubmit}>
+          <input value={textValue} onChange={handleTextChange} type="text" placeholder="Task name"/>
           <button>Create Task</button>
         </form>
 
